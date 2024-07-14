719a34bb56fbfcf57cec476dd4bdf54d54be54d6
yarynots <110288411+yarynots@users.noreply.github.com>
2024-07-14 17:08:03 +0000

    Connectionstring SQL to MYSQL

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if(builder.Environment.IsDevelopment())
  else
  {
      builder.Services.AddDbContext<MyDatabaseContext>(options =>
-         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_MYSQL_CONNECTIONSTRING")));
      builder.Services.AddStackExchangeRedisCache(options =>
      {
      options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
